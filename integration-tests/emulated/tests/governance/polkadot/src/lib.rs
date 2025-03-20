f6d40550a79255f51351376490aedd7063f7015f
Karol Kokoszka <karol@parity.io>
2025-03-20 21:57:27 +0100

    [Governance tests] can_authorize_upgrade_for_system_chains initial impl

--- integration-tests/emulated/tests/governance/polkadot/src/lib.rs
+++ integration-tests/emulated/tests/governance/polkadot/src/lib.rs
@@ -14,20 +14,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use emulated_integration_tests_common::impls::{assert_expected_events, bx, TestExt};
+use emulated_integration_tests_common::impls::{
+	assert_expected_events, bx, Parachain, RelayChain, TestExt,
+};
 use frame_support::assert_ok;
 use integration_tests_helpers::Chain;
-use polkadot_system_emulated_network::CollectivesPolkadotPara as CollectivesPolkadot;
+use polkadot_system_emulated_network::{
+	AssetHubPolkadotPara as AssetHubPolkadot, BridgeHubPolkadotPara as BridgeHubPolkadot,
+	CollectivesPolkadotPara as CollectivesPolkadot,
+};
 use sp_runtime::traits::Dispatchable;
-use xcm::latest::prelude::*;
-use xcm::{VersionedLocation, VersionedXcm};
+use xcm::{latest::prelude::*, VersionedLocation, VersionedXcm};
 
 #[cfg(test)]
 mod open_gov_on_asset_hub;
 #[cfg(test)]
 mod open_gov_on_relay;
 
-/// CollectivesPolkadot dispatches `pallet_xcm::send` with `OriginKind:Xcm` to the dest with encoded whitelist call.
+/// CollectivesPolkadot dispatches `pallet_xcm::send` with `OriginKind:Xcm` to the dest with encoded
+/// whitelist call.
 pub fn collectives_send_whitelist(
 	dest: Location,
 	encoded_whitelist_call: impl FnOnce() -> Vec<u8>,
--- integration-tests/emulated/tests/governance/polkadot/src/open_gov_on_relay.rs
+++ integration-tests/emulated/tests/governance/polkadot/src/open_gov_on_relay.rs
@@ -12,21 +12,21 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-use crate::collectives_send_whitelist;
+use crate::{collectives_send_whitelist, *};
 use codec::Encode;
 use emulated_integration_tests_common::xcm_emulator::{Chain, TestExt};
-use frame_support::dispatch::DispatchResultWithPostInfo;
-use frame_support::{assert_err, assert_ok};
+use frame_support::{assert_err, assert_ok, dispatch::DispatchResultWithPostInfo};
 use polkadot_runtime::governance::pallet_custom_origins::Origin;
 use polkadot_system_emulated_network::{
 	polkadot_emulated_chain::PolkadotRelayPallet as PolkadotPallet, PolkadotRelay as Polkadot,
 };
 use sp_core::H256;
-use sp_runtime::traits::Dispatchable;
-use sp_runtime::traits::Hash;
-use sp_runtime::DispatchError;
-use xcm::latest::Location;
+use sp_runtime::{
+	traits::{Dispatchable, Hash},
+	DispatchError,
+};
+// use xcm::latest::Location;
+// use xcm::latest::prelude::*;
 
 fn store_preimage(call: polkadot_runtime::RuntimeCall) -> H256 {
 	Polkadot::execute_with(|| {
@@ -119,7 +119,119 @@ fn can_authorize_upgrade_for_relaychain() {
 
 #[test]
 fn can_authorize_upgrade_for_system_chains() {
-	// TODO: upgrage AssetHub
-	// TODO: upgrage Collectives
-	// TODO: upgrage BridgeHub
+	let code_hash = [1u8; 32].into();
+	type Runtime = <Polkadot as Chain>::Runtime;
+
+	let asset_hub_location = Polkadot::child_location_of(AssetHubPolkadot::para_id());
+	let collectives_location = Polkadot::child_location_of(CollectivesPolkadot::para_id());
+	let bridgehub_location = Polkadot::child_location_of(BridgeHubPolkadot::para_id());
+
+	let asset_hub_authorize_upgrade_call =
+		asset_hub_polkadot_runtime::RuntimeCall::System(frame_system::Call::authorize_upgrade {
+			code_hash,
+		});
+	let collectives_authorize_upgrade_call =
+		collectives_polkadot_runtime::RuntimeCall::System(frame_system::Call::authorize_upgrade {
+			code_hash,
+		});
+	let bridge_hub_authorize_upgrade_call =
+		bridge_hub_polkadot_runtime::RuntimeCall::System(frame_system::Call::authorize_upgrade {
+			code_hash,
+		});
+
+	let authorize_upgrade =
+		polkadot_runtime::RuntimeCall::Utility(pallet_utility::Call::<Runtime>::force_batch {
+			calls: vec![
+				polkadot_runtime::RuntimeCall::XcmPallet(pallet_xcm::Call::send {
+					dest: bx!(VersionedLocation::from(asset_hub_location)),
+					message: bx!(VersionedXcm::from(Xcm(vec![
+						UnpaidExecution { weight_limit: Unlimited, check_origin: None },
+						Transact {
+							origin_kind: OriginKind::Superuser,
+							require_weight_at_most: Weight::from_parts(5_000_000_000, 500_000),
+							call: asset_hub_authorize_upgrade_call.encode().into(),
+						}
+					]))),
+				}),
+				polkadot_runtime::RuntimeCall::XcmPallet(pallet_xcm::Call::send {
+					dest: bx!(VersionedLocation::from(collectives_location)),
+					message: bx!(VersionedXcm::from(Xcm(vec![
+						UnpaidExecution { weight_limit: Unlimited, check_origin: None },
+						Transact {
+							origin_kind: OriginKind::Superuser,
+							require_weight_at_most: Weight::from_parts(5_000_000_000, 500_000),
+							call: collectives_authorize_upgrade_call.encode().into(),
+						}
+					]))),
+				}),
+				polkadot_runtime::RuntimeCall::XcmPallet(pallet_xcm::Call::send {
+					dest: bx!(VersionedLocation::from(bridgehub_location)),
+					message: bx!(VersionedXcm::from(Xcm(vec![
+						UnpaidExecution { weight_limit: Unlimited, check_origin: None },
+						Transact {
+							origin_kind: OriginKind::Superuser,
+							require_weight_at_most: Weight::from_parts(5_000_000_000, 500_000),
+							call: bridge_hub_authorize_upgrade_call.encode().into(),
+						}
+					]))),
+				}),
+			],
+		});
+
+	// bad origin
+	let invalid_origin: polkadot_runtime::RuntimeOrigin = Origin::StakingAdmin.into();
+	// ok origin
+	let ok_origin: polkadot_runtime::RuntimeOrigin = Origin::WhitelistedCaller.into();
+
+	// store preimage
+	let call_hash = store_preimage(authorize_upgrade.clone());
+
+	// Err - when dispatch non-whitelisted
+	assert_err!(
+		dispatch_whitelisted_call_with_preimage(authorize_upgrade.clone(), ok_origin.clone()),
+		DispatchError::Module(sp_runtime::ModuleError {
+			index: 23,
+			error: [3, 0, 0, 0],
+			message: Some("CallIsNotWhitelisted")
+		})
+	);
+
+	// whitelist
+	collectives_send_whitelist(Location::parent(), || {
+		polkadot_runtime::RuntimeCall::Whitelist(
+			pallet_whitelist::Call::<Runtime>::whitelist_call { call_hash },
+		)
+		.encode()
+	});
+
+	// Err - when dispatch wrong origin
+	assert_err!(
+		dispatch_whitelisted_call_with_preimage(authorize_upgrade.clone(), invalid_origin),
+		DispatchError::BadOrigin
+	);
+
+	// check before
+	AssetHubPolkadot::execute_with(|| {
+		assert!(asset_hub_polkadot_runtime::System::authorized_upgrade().is_none())
+	});
+	CollectivesPolkadot::execute_with(|| {
+		assert!(collectives_polkadot_runtime::System::authorized_upgrade().is_none())
+	});
+	BridgeHubPolkadot::execute_with(|| {
+		assert!(bridge_hub_polkadot_runtime::System::authorized_upgrade().is_none())
+	});
+
+	// ok - authorized
+	assert_ok!(dispatch_whitelisted_call_with_preimage(authorize_upgrade, ok_origin));
+
+	// check after - authorized
+	AssetHubPolkadot::execute_with(|| {
+		assert!(asset_hub_polkadot_runtime::System::authorized_upgrade().is_some())
+	});
+	CollectivesPolkadot::execute_with(|| {
+		assert!(collectives_polkadot_runtime::System::authorized_upgrade().is_some())
+	});
+	BridgeHubPolkadot::execute_with(|| {
+		assert!(bridge_hub_polkadot_runtime::System::authorized_upgrade().is_some())
+	});
 }
